6424b63216c7fe9b80e6ab35284852896f047e48
thepersonilike13 <a.allairjoshua@gmail.com>
2025-05-10 20:06:09 +0530

    3rd time

---
+++ test.py
@@ -0,0 +1,17 @@
+import pickle
+
+with open("./faiss_index/index.pkl", "rb") as f:
+    data = pickle.load(f)
+
+
+      
+    
+    """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+    Answer:
+
+
+    """
